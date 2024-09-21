2bb6d90e79b77118c0801594f1cac4c29997b113
Neethu-Muthu <22m014@nssce.ac.in>
2024-09-21 13:01:08 +0530

    navbar

--- frontend/src/Pages/Navbar.jsx
+++ frontend/src/Pages/Navbar.jsx
@@ -1,41 +1,40 @@
 // Navbar.js
 import React from "react";
-import { Link } from "react-router-dom"; // If you're using React Router
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 p-4">
+    <nav className="bg-gray-800 p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-lg font-bold">
           <Link to="/">Learning Tracker</Link>
         </div>
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/" className="text-white hover:text-blue-300">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/courses" className="text-white hover:text-blue-300">
-              Courses
-            </Link>
-          </li>
-          <li>
-            <Link to="/certificates" className="text-white hover:text-blue-300">
-              Certificates
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className="text-white hover:text-blue-300">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="text-white hover:text-blue-300">
-              Contact
-            </Link>
-          </li>
-        </ul>
+        <div className="flex space-x-4">
+          <Link
+            to="/"
+            className="text-white hover:bg-blue-600 px-4 py-2 rounded"
+          >
+            Home
+          </Link>
+          <Link
+            to="/sign-up"
+            className="text-white hover:bg-blue-600 px-4 py-2 rounded"
+          >
+            Sign Up
+          </Link>
+          <Link
+            to="/login"
+            className="text-white hover:bg-blue-600 px-4 py-2 rounded"
+          >
+            Login
+          </Link>
+          <Link
+            to="/certificate"
+            className="text-white hover:bg-blue-600 px-4 py-2 rounded"
+          >
+            Certificate
+          </Link>
+        </div>
       </div>
     </nav>
   );
